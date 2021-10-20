7a3d9dbef269272afc2e993805b38928fd74512a
iimorimizuho <tanaka.mizuho.com@gmail.com>
2021-10-20 15:01:37 +0900

    first commit

---
+++ user.rb
@@ -0,0 +1,14 @@
+class User
+  # def name= (name)
+  #   @name = name
+  # end
+  # def name
+  #   @name
+  # end
+  attr_accessor :name, :address, :email
+
+  def profile
+    "#{name} (#{address})"
+  end
+
+end
